3047889dacc37a01004084fc18018c284998eab8
Enmanuel Rodriguez <enmaundostres@gmail.com>
2025-02-09 15:07:35 -0400

    wip: general changes

--- install.sh
+++ install.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
 # Clonar los repositorios
-git clone -b develop https://github.com/enmanuelrdgz/quickpolls-core.git
+git clone https://github.com/enmanuelrdgz/quickpolls-core.git
 if [ $? -ne 0 ]; then
   echo "Error: No se pudo clonar quickpolls-core."
   exit 1
 fi
-cd quickpolls-core
-git checkout develop
 
-git clone -b test https://github.com/enmanuelrdgz/quickpolls-client.git
+git clone https://github.com/enmanuelrdgz/quickpolls-client.git
 if [ $? -ne 0 ]; then
   echo "Error: No se pudo clonar quickpolls-client."
   exit 1
 fi
-cd quickpolls-client
-git checkout test
-
-cd ..
 
 # Crear una red de docker
 docker network create quickpolls-network
