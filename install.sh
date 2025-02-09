59db38b8a565856da7f2a3f6584d92e2cf43ce8e
Enmanuel Rodriguez <enmaundostres@gmail.com>
2025-02-09 13:31:59 -0400

    wip: add new files

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Clonar los repositorios
+git clone -b develop https://github.com/enmanuelrdgz/quickpolls-core.git
+if [ $? -ne 0 ]; then
+  echo "Error: No se pudo clonar quickpolls-core."
+  exit 1
+fi
+
+git clone -b develop https://github.com/enmanuelrdgz/quickpolls-client.git
+if [ $? -ne 0 ]; then
+  echo "Error: No se pudo clonar quickpolls-client."
+  exit 1
+fi
+
+# Construir y levantar los contenedores
+docker-compose up --build
+if [ $? -ne 0 ]; then
+  echo "Error: No se pudo levantar los contenedores con docker-compose."
+  exit 1
+fi
+
