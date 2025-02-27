c992efe611e01077720575985adf864c66a7f88c
Enmanuel Rodriguez <enmaundostres@gmail.com>
2025-02-27 00:00:00 -0400

    fix conditional in start.sh if statements

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clonar el repositorio del backend
-if [-d ./backend-app]; then 
+if [ -d ./backend-app ]; then 
   echo "El directorio \"backend-app\" ya existe."
 else
   git clone https://github.com/enmanuelrdgz/backend-app.git
@@ -12,7 +12,7 @@ else
 fi
 
 # Clonar el repositorio del frontend
-if [-d ./frontend-app]; then 
+if [ -d ./frontend-app ]; then 
   echo "El directorio \"frontend-app\" ya existe."
 else
   git clone https://github.com/enmanuelrdgz/frontend-app.git
